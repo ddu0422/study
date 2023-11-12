79ae767819dfb6a9e29a5c46d73732ac81cd8514
ddu0422 <ddu0422@naver.com>
2023-11-12 20:07:38 +0900

    만들면서 배우는 아키텍처: 프로젝트 초기화

---
+++ books/만들면서_배우는_클린_아키텍처/code/architecture/src/test/java/per/study/architecture/ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package per.study.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
